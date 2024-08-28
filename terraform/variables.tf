91b804c373ca32f04c234a4b936f53b8c2895328
wanwar@cyberciso.ca <wanwar@cyberciso.ca>
2024-08-27 23:16:40 -0400

    corrected terraform workflow

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -44,7 +44,7 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0c6d358ee9e264ff1" // Replace with the latest AMI ID for your region
+  default     = "ami-0c6d358ee9e264ff1"
 }
 
 variable "instance_type" {
